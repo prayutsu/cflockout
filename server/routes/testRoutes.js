8709cbecf0978d52f5fd13ec66328c04d5ea43ed
Abhay Garg <abhay.garg@cred.club>
2023-03-25 00:26:46 +0530

    Configure test email route

--- server/routes/testRoutes.js
+++ server/routes/testRoutes.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const router = express.Router();
+const { VERIFY_EMAIL } = require("../config/constants");
+const { sendMail } = require("../services/emailService");
 
 router.get(
   "/test",
   asyncHandler(async (req, res) => {
     res.status(200).json({ sucess: "Hello world!!" });
+  })
+);
+
+router.post(
+  "/email",
+  asyncHandler(async (req, res) => {
+    const { user } = req.body;
+    await sendMail(user, VERIFY_EMAIL)
+      .then(() =>
+        res.status(200).json({ message: `Email sent to ${user.email}` })
+      )
+      .catch((err) => {
+        res
+          .status(400)
+          .json({
+            message: `Error sending email to ${user.email}`,
+            error: err,
+          });
+      });
   })
 );
 
